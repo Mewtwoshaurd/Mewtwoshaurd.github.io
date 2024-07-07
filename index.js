d08aea7b3cbffab952d29f3af2a5106ec2225719
mewtwoshaurd <mewtwoshard@gmail.com>
2024-07-06 23:33:16 -0500

    chore: updated comments, removed unused styling, cleaned up js

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+/*
+    functionality for slideshow
+*/
+let slides = null;
 let index = 0;
 
-function nextSlide(i)
-{
+/*
+    event lisenter to get all slides on window load once
+*/
+window.addEventListener("load", (event) => {
+    slides = document.getElementsByClassName("slide");
+});
+
+/*
+ easy next slide and current slide button clicks
+*/
+function nextSlide(i) {
     showSlide(index += i);
 }
 
-function currentSlide(i)
-{
+function currentSlide(i) {
     showSlide(index = i);
 }
 
-function showSlide(i)
-{
+/*
+    showSlide will update CSS to show current slide
+*/
+function showSlide(i) {
     let n;
-    let slides = document.getElementsByClassName("slide");
     if (i > slides.length)
     {
         index = 1
@@ -28,6 +41,11 @@ function showSlide(i)
     }
     slides[index-1].style.display = "flex";
 }
+
+/*
+    functionality for clickable menu; event listener on click after load occured
+    so that we know there is something to click
+*/
 
 window.addEventListener("load", (event) => {
     document.getElementById("collapse").addEventListener("click", function dropDown() 
